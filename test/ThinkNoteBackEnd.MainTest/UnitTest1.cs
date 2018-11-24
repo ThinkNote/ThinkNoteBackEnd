04047c89109c5dc68dd4e3276f3ea0709f2bf4fd
NemesissLin <353388639@qq.com>
2018-11-25 00:05:04 +0800

    first commit to build up repo

---
+++ test/ThinkNoteBackEnd.MainTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ThinkNoteBackEnd.MainTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
