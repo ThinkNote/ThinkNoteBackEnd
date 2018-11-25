3560a5c3bddadcd4496451f90d7ab4313a319287
NemesissLin <353388639@qq.com>
2018-11-25 20:42:43 +0800

    添加appveyor持续集成配置文件

--- src/ThinkNoteBackEnd/Startup.cs
+++ src/ThinkNoteBackEnd/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -39,7 +40,6 @@ namespace ThinkNoteBackEnd
             {
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseMvc();
         }
