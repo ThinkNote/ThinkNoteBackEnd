305fa6b3349ac3c28d92df15e2e4b737e3257283
LinZong <353388639@qq.com>
2018-11-26 20:40:17 +0800

    add snowflake:

--- src/ThinkNoteBackEnd.User/UIDWorker/InvalidSystemClock.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace ThinkNoteBackEnd.User
-{
-    public class InvalidSystemClock : Exception
-    {      
-        public InvalidSystemClock(string message) : base(message) { }
-    }
-}
